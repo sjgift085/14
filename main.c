aad8307680644424984ec74b7b0c3bfebed24f97
sjgift085 <77606778+sjgift085@users.noreply.github.com>
2025-12-04 15:24:51 +0900

    실습 2

--- main.c
+++ main.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct Book{
+			 int number;
+			 char title[10];
+};
+
 int main(int argc, char *argv[])
 {
- 		char *pc = NULL; 
-		int i = 0; 
+ 		struct Book *p;
+ 		
+ 		p = (struct Book*)malloc(2 * sizeof(struct Book));
+ 		
+ 		if(p==NULL){
+			printf("메모리 할당 오류\n");
+			return;
+			}
 		
-		pc = (char*)malloc(100*sizeof(char)); if (pc == NULL) { 
-		printf("메모리할당오류\n"); 
-		exit(1); 
-		}
+		p->number = 1;
+		strcpy(p->title, "C programming");
 		
-		for (i=0;i<26;i++){ 
-				pc[i] = 'a'+i; 
-				}
-		pc[i] = 0; 
-		printf("%s\n", pc);
-		free(pc); 
-  
-  system("PAUSE");	
-  return 0;
+		(p+1)->number = 2;
+		strcpy((p+1)->title, "Electronics");
+		
+		free(p);
+	  system("PAUSE");	
+    return;
 }
